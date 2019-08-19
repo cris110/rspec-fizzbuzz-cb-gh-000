2c660cff8242bf908962bceba56854bbdf831624
Cristiano Moreira <cristiano.moreira01@fatec.sp.gov.br>
2019-08-19 00:29:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,11 @@
 def fizzbuzz(num)
   if (num % 3) == 0 && (num % 5) == 0
     return "fizzbuzz"
-  elsif num % 3 == 0
+  elsif (num % 3) == 0
     return "fizz"
-  elsif num % 5 == 0
+  elsif (num % 5) == 0
     return "buzz"
   else
     nil
 end
+fizzbuzz(5)
